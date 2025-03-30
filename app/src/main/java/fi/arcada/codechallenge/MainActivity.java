f6a64055f20e5229829aaa163381551a9dd1a9fc
BabyxRabbit <linda.lindholm3@gmail.com>
2025-03-30 16:36:00 +0300

    CodeChallengeMean

--- app/src/main/java/fi/arcada/codechallenge/MainActivity.java
+++ app/src/main/java/fi/arcada/codechallenge/MainActivity.java
@@ -2,6 +2,11 @@ package fi.arcada.codechallenge;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -10,10 +15,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
+    private Button button;
+    private ArrayList<Double> list;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textView = findViewById(R.id.hiii);
+        button = findViewById(R.id.button);
+
+        textView.setText("Min app funkar!");
+
+        list = new ArrayList<>();
+        list.add(4.0);
+        list.add(16.0);
+        list.add(45.0);
+
+        button.setOnClickListener(v -> {
+            Double mean = calcMean(list);
+            textView.setText("" + mean);
+        });
+    }
+
+    // Method to calculate the median
+    public static Double calcMean(ArrayList<Double> list) {
+        double sum = 0.0;
+        for (Double num : list) {
+            sum += num;
+        }
+        return sum / list.size();
     }
 }
